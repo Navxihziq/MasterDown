dda11064c351ec6f6bf406fa4568d02a56f3dda4
unknown <zhixuanqi@outlook.com>
2022-07-19 21:24:44 +0800

    initial commit from local

---
+++ entry/src/main/java/com/zhixuanqi/masterdown/MyApplication.java
@@ -0,0 +1,10 @@
+package com.zhixuanqi.masterdown;
+
+import ohos.aafwk.ability.AbilityPackage;
+
+public class MyApplication extends AbilityPackage {
+    @Override
+    public void onInitialize() {
+        super.onInitialize();
+    }
+}
---
+++ entry/src/test/java/com/zhixuanqi/masterdown/ExampleTest.java
@@ -0,0 +1,9 @@
+package com.zhixuanqi.masterdown;
+
+import org.junit.Test;
+
+public class ExampleTest {
+    @Test
+    public void onStart() {
+    }
+}
