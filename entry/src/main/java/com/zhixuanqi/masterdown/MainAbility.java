06e6726213572b929e6478d035936a18bdc9ca4f
unknown <zhixuanqi@outlook.com>
2022-07-19 22:59:39 +0800

    Made the main ability a fraction ability to fulfill the functionality of navigation bar

--- entry/src/main/java/com/zhixuanqi/masterdown/MainAbility.java
+++ entry/src/main/java/com/zhixuanqi/masterdown/MainAbility.java
@@ -2,12 +2,21 @@ package com.zhixuanqi.masterdown;
 
 import com.zhixuanqi.masterdown.slice.MainAbilitySlice;
 import ohos.aafwk.ability.Ability;
+import ohos.aafwk.ability.fraction.FractionAbility;
 import ohos.aafwk.content.Intent;
+import ohos.agp.components.TabList;
 
-public class MainAbility extends Ability {
+public class MainAbility extends FractionAbility {
     @Override
     public void onStart(Intent intent) {
         super.onStart(intent);
         super.setMainRoute(MainAbilitySlice.class.getName());
+        showBottomNavIcons();
+    }
+
+    public void showBottomNavIcons(){
+        TabList tabList = (TabList) findComponentById(ResourceTable.Id_bottom_nav);
+        TabList.Tab tab = tabList.new Tab(getContext());
+        tab.setText("Test");
     }
 }
